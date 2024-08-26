c9b07bc02d29df7fd132d84a741815de6b4f9d04
youseffswd <yousef.sayed.work@gmail.com>
2024-08-26 13:22:55 +0300

    fix navbar

--- components/Header/NavLinks.js
+++ components/Header/NavLinks.js
@@ -5,14 +5,14 @@ const NavLinks = ({ open }) => {
     const translate =
         (open ? "translate-x-[0%]" : "translate-x-[100%]") +
         " lg:translate-x-[0%]";
-    const position = `absolute lg:static z-10 top-0 right-0`;
+    const position = `fixed lg:static z-10 top-0 right-0`;
     const background = `bg-[rgba(255,255,255,.05)] backdrop-blur-lg lg:bg-transparent lg:backdrop-none`;
     const sizing = `h-screen lg:h-auto w-64 md:w-96 lg:w-full py-32 lg:pt-0 lg:pb-0`;
     const lg = ``;
     const xl = ``;
     return (
         <nav
-            className={`transition -mt-6 ${translate} overflow-y-auto lg:overflow-y-visible  ${position} ${background} text-center  ${sizing} ${lg} ${xl}`}
+            className={`transition ${translate} overflow-y-auto lg:overflow-y-visible  ${position} ${background} text-center  ${sizing} ${lg} ${xl}`}
         >
             <ul className="text-lg h-full md:text-xl  font-semibold flex gap-5 flex-col lg:flex-row lg:items-center justify-center lg:justify-around lg:ml-16 xl:ml-32 lg:gap-5 xl:gap-10 ">
                 <li>
